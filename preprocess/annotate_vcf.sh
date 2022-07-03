e1ca3e42079609d3c1d6b8e39673a0202227dbbd
fkbyf14 <a462611336@gmail.com>
2022-07-03 13:14:51 +0400

    set missing id

--- preprocess/annotate_vcf.sh
+++ preprocess/annotate_vcf.sh
@@ -4,3 +4,4 @@ bcftools index allvars_dbsnp.vcf.gz
 bcftools view example_38.vcf -Oz -o example_38.vcf.gz
 bcftools index example_38.vcf.gz
 bcftools annotate -c ID -a allvars_dbsnp.vcf.gz example_38.vcf.gz -o example_allvars_dbsnp.vcf.gz
+bcftools annotate --set-id +'%CHROM\_%POS' example_allvars_dbsnp.vcf.gz -o example_allvars_dbsnp_chrompos.vcf.gz
