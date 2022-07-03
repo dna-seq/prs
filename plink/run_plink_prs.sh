af4cab73545550f556cb166a931bbb7fdf67f3e3
fkbyf14 <a462611336@gmail.com>
2022-07-03 12:56:28 +0400

    annotating vcf with bcftools

--- plink/run_plink_prs.sh
+++ plink/run_plink_prs.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 cut -f 2 pickard_allvars.bim | sort | uniq -d > 1.dups
-plink --bfile pickard_allvars --exclude 1.dups --score ../PGS001298_chrpos.raw --allow-extra-chr
+# https://www.cog-genomics.org/plink/1.9/score
+# By default, final scores are averages of valid per-allele scores. The 'sum' modifier causes sums to be reported instead
+plink --bfile pickard_allvars --exclude 1.dups --score ../PGS001298_chrpos.raw sum --allow-extra-chr
---
+++ preprocess/annotate_vcf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# allvars_dbsnp.vcf.gz can be downloaded from https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/
+bcftools index allvars_dbsnp.vcf.gz
+bcftools view example_38.vcf -Oz -o example_38.vcf.gz
+bcftools index example_38.vcf.gz
+bcftools annotate -c ID -a allvars_dbsnp.vcf.gz example_38.vcf.gz -o example_allvars_dbsnp.vcf.gz
